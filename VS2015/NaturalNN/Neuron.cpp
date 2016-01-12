8abdb329de16d5f7aab41a3142340b3f0e619de0
Boris Lazarov <vordhosbn.bg@gmail.com>
2016-01-12 17:23:12 +0200

    Made Neuron value get/set/reset thread safe and expanded Connection with event for having been run trough

--- VS2015/NaturalNN/Neuron.cpp
+++ VS2015/NaturalNN/Neuron.cpp
@@ -7,3 +7,21 @@ Neuron::Neuron()
 Neuron::~Neuron()
 {
 }
+
+double Neuron::GetValue()
+{
+    std::lock_guard<mutex> lock(neuronValue_mutex);
+    return neuronValue;
+}
+
+void Neuron::AddToValue(double addition)
+{
+    std::lock_guard<mutex> lock(neuronValue_mutex);
+    neuronValue += addition;
+}
+
+void Neuron::ResetValue()
+{
+    std::lock_guard<mutex> lock(neuronValue_mutex);
+    neuronValue = 0;
+}
--- VS2015/NaturalNN/Neuron.h
+++ VS2015/NaturalNN/Neuron.h
@@ -3,6 +3,7 @@
 #include <vector>
 #include <memory>
 #include "Connection.h"
+#include <mutex>
 
 using namespace std;
 class Connection;
@@ -12,8 +13,17 @@ public:
     Neuron();
     ~Neuron();
 
+    double GetValue();
+    void AddToValue(double addition);
+    void ResetValue();
+
 private:
-    vector<shared_ptr<Connection>> ListOfConnections;
+    vector<shared_ptr<Connection>> ConnectionsFrom;
+    vector<shared_ptr<Connection>> ConnectionsTo;
+    
+    double neuronValue;
+    mutex neuronValue_mutex;
+
 };
 
 #endif NEURON_H
