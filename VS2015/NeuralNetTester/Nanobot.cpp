b4eb1f4972b1f7cf6b32c1cc0bd123cf3b29eb1e
Boris Lazarov <vordhosbn.bg@gmail.com>
2016-01-21 14:42:50 +0200

    StartMovement() and StopMovement() added for debug purposes

--- VS2015/NeuralNetTester/Nanobot.cpp
+++ VS2015/NeuralNetTester/Nanobot.cpp
@@ -2,17 +2,31 @@
 
 
 
-Nanobot::Nanobot() : GameObject(Vector2D(0,0), Vector2D(0,0), Vector2D(0,0), 100, 0.3, 32, ".\\res\\pictures\\nanobot64.png")
+Nanobot::Nanobot() : GameObject(Vector2D(0,0), Vector2D(0,0), Vector2D(0,0), 100, 0.3, 32, ".\\res\\pictures\\nanobot64.png"), is_moving(false)
 {
 }
 
 
-Nanobot::~Nanobot()
+Nanobot::~Nanobot() 
 {
 }
 
 void Nanobot::Tick(double dT)
 {
-    ApplyForceInDirection(0.2);
+    if (is_moving)
+    {
+        ApplyForceInDirection(0.2);
+        ApplyRotation(10);
+    }
+}
+
+void Nanobot::StartMovement()
+{
+    is_moving = true;
+}
+
+void Nanobot::StopMovement()
+{
+    is_moving = false;
 }
 
--- VS2015/NeuralNetTester/Nanobot.h
+++ VS2015/NeuralNetTester/Nanobot.h
@@ -9,6 +9,10 @@ public:
     ~Nanobot();
 
     void Tick(double dT) override;
+    void StartMovement();
+    void StopMovement();
+private:
+    bool is_moving;
 };
 
 #endif // NANOBOT_H
